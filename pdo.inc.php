c0d155d014ce871e3a24fccfb4e731867a408e5d
Dana Engel <engel.dana.mail@gmail.com>
2018-05-09 13:08:48 +0200

    customize and change header content

--- pdo.inc.php
+++ pdo.inc.php
@@ -1,12 +1,23 @@
 <?php
-/*** mysql hostname ***/
-$hostname = 'localhost';
+$hostname = 'localhost'; 	// db hostname
+$username = 'root';		 	// db username
+$password = '';			 	// db password
+$dbname = 'macmanu_groupL';	// db name
 
-/*** mysql username ***/
-$username = 'root';
+echo "<!DOCTYPE html>\n";
+echo "<html>\n";
+echo "<head>\n";
+echo "<meta charset='UTF-8'>\n";
+echo "<meta name='viewport' content='width=device-width, initial-scale=1.0'/>\n";
 
-/*** mysql password ***/
-$password = '';
+echo "<link rel='stylesheet' type='text/css' href='css/style.css'>\n";
+echo "<link rel='stylesheet' type='text/css' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css'>\n";
 
-$dbname = 'medizininformatik';
+echo "<script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js'></script>\n";
+echo "<script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js'></script>\n";
+echo "<script type='text/javascript' src='https://code.jquery.com/jquery-3.3.1.min.js'></script>\n";
+
+echo '<title>CHEFFE</title>';
+echo "</head>\n";
+echo "<body>\n";
 ?>
