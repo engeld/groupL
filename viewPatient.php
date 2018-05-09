79522a3108dbfaba97f8bf1ddd96a694b62b0a5e
Dana Engel <engel.dana.mail@gmail.com>
2018-05-09 15:13:43 +0200

    reorder the pages

--- viewPatient.php
+++ viewPatient.php
@@ -14,53 +14,37 @@ include('pdo.inc.php');
 try {
     $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
     $patientID=0;
+
     if(isset($_GET['id'])){
       $patientID = (int)($_GET['id']);
     }
     if($patientID >0){
+      $statement0 = $dbh->prepare("SELECT name, first_name FROM patient WHERE patient.patientID = :patientID");
+      $statement0->bindParam(':patientID', $patientID, PDO::PARAM_INT);
+      $result0 = $statement0->execute();
 
-      $sql0 = "SELECT name, first_name
-  FROM patient
-  WHERE patient.patientID = :patientID";
+      while($line = $statement0->fetch()){
+        echo "<h1> Patient : ".$line['first_name']."  ".$line['name']."</h1>";
+        echo "<br>\n";
+      }
 
-    $statement0 = $dbh->prepare($sql0);
-    $statement0->bindParam(':patientID', $patientID, PDO::PARAM_INT);
-    $result0 = $statement0->execute();
+      $sql = "SELECT name, first_name, value, time, sign_name FROM patient, vital_sign, sign WHERE patient.patientID = vital_sign.patientID AND vital_sign.signID = sign.signID  AND patient.patientID = :patientID";
 
-    while($line = $statement0->fetch()){
-      echo "<h1> Patient : ".$line['first_name']."  ".$line['name']."</h1>";
+      $statement = $dbh->prepare($sql);
+      $statement->bindParam(':patientID', $patientID, PDO::PARAM_INT);
+      $result = $statement->execute();
 
-      echo "<br>\n";
-    }
-
-
-      /*** echo a message saying we have connected ***/
-      $sql = "SELECT name, first_name, value, time, sign_name
-  FROM patient, vital_sign, sign
-  WHERE patient.patientID = vital_sign.patientID
-    AND vital_sign.signID = sign.signID 
-    AND patient.patientID = :patientID";
-
-    $statement = $dbh->prepare($sql);
-    $statement->bindParam(':patientID', $patientID, PDO::PARAM_INT);
-    $result = $statement->execute();
-
-    while($line = $statement->fetch()){
-      echo $line['sign_name']." = ".$line['value']. " at ".$line['time'];
-
-      echo "<br>\n";
-    }
+      while($line = $statement->fetch()){
+        echo $line['sign_name']." = ".$line['value']. " at ".$line['time'];
+        echo "<br>\n";
+      }
 
     }
     else{
       echo "<h1>The patient does not exist</h1>";
     }
-
-    //$dbh = null;
 }
-catch(PDOException $e)
-{
-
+catch(PDOException $e) {
     /*** echo the sql statement and error message ***/
     echo $e->getMessage();
 }
