dc7edfb8fc0dce8d177b83d7f4160eb5fa8a018a
Manuel Petitat <manuel@Manuels-MacBook-Pro.local>
2018-05-13 22:17:21 +0200

    redirecting to login after logout

--- logout.php
+++ logout.php
@@ -5,4 +5,11 @@ unset($_SESSION['user']);
 
 ?>
 <h1>Logout done</h1>
-<a href="index.php">Back to login page</a>
+<p> redirecting to Login</p>
+<p>Falls Sie nicht auf das Login weitergeleitet werden benutzen Sie bitten diesen Link</p>
+<a href="index.php">Login</a>
+
+<?php
+sleep(1);
+header("Location: index.php");
+?>
